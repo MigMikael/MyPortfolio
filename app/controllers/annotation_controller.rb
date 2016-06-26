7c4220c17881a8e972f2c153a18ac4ab9b69e61c
Nanthipath <omliler_man@hotmail.com>
2016-06-26 14:08:38 +0700

    add post request

--- app/controllers/annotation_controller.rb
+++ app/controllers/annotation_controller.rb
@@ -1,12 +1,21 @@
 class AnnotationController < ApplicationController
 require 'json'
+require 'net/http'
+require "uri"
+
   def call_java_check
     output = `java -cp "/Users/manny/Documents/Work/ruby/CodeRoomDev/app/controllers/build/classes" java_parser.CheckAnnotation`
     text_file = File.new('/Users/manny/Documents/Work/ruby/CodeRoomDev/app/controllers/json_file.txt', "r").read
     # puts JSON.parse(text_file)
     # puts @text
     # @text = text_file.to_json
-    @text= {class:[{modifier:"deafult",name:"A", member:[{modifier:"public",datatype:"int",name:"x"},{modifier:"private",datatype:"double",name:"man"}],method:[modifier:"public",returntype:"string",name:"printV1",parms:[{datatype:"string[]",name:"x"},{datatype:"int",name:"y"},]]}]}
+
+    x = Net::HTTP.post_form(URI.parse('http://localhost:888/api/problems_analysis'), text_file.as_json)
+    puts x.body
+
+    @text = {class:[{modifier:"deafult",name:"A", member:[{modifier:"public",datatype:"int",name:"x"},{modifier:"private",datatype:"double",name:"man"}],method:[modifier:"public",returntype:"string",name:"printV1",parms:[{datatype:"string[]",name:"x"},{datatype:"int",name:"y"},]]}]}
+
+
 
     respond_to do |format|
       # @text = text_file.to_json
